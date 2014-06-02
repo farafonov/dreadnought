6d3a7d4cfbf0e93c6e8f154b8b0021322d08fca9
Dmitri Farafonov <farafonov@gmail.com>
2014-06-02 12:11:52 -0400

    Test setup

---
+++ dreadnought-encryption/src/test/java/com/dreadnought/encryption/SimpleEncryptDecryptTest.java
@@ -0,0 +1,16 @@
+package com.dreadnought.encryption;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Dmitri Farafonov on 6/2/14.
+ */
+public class SimpleEncryptDecryptTest {
+    @Test
+    public void testName() throws Exception {
+
+
+    }
+
+
+}
