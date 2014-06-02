b943e084a063a5394e53a73d9ae55d147d736c72
Dmitri Farafonov <farafonov@gmail.com>
2014-06-02 12:24:55 -0400

    Test setup

---
+++ dreadnought-encryption/src/main/java/com/dreadnought/encryption/EncryptionUtil.java
@@ -0,0 +1,14 @@
+package com.dreadnought.encryption;
+
+/**
+ * Created by boss on 6/2/14.
+ */
+public class EncryptionUtil {
+    public static byte[] encrypt(byte[] blah) {
+        return new byte[0];
+    }
+
+    public static byte[] decrypt(byte[] encrypted) {
+        return new byte[0];
+    }
+}
--- dreadnought-encryption/src/test/java/com/dreadnought/encryption/SimpleEncryptDecryptTest.java
+++ dreadnought-encryption/src/test/java/com/dreadnought/encryption/SimpleEncryptDecryptTest.java
@@ -1,5 +1,6 @@
 package com.dreadnought.encryption;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
@@ -7,9 +8,11 @@ import org.testng.annotations.Test;
  */
 public class SimpleEncryptDecryptTest {
     @Test
-    public void testName() throws Exception {
-
-
+    public void testEncryptBlah() throws Exception {
+        byte[] blah = "blah".getBytes("UTF-8");
+        byte[] encrypted = EncryptionUtil.encrypt(blah);
+        byte[] decrypted = EncryptionUtil.decrypt(encrypted);
+        Assert.assertEquals("blah", new String(decrypted, "UTF-8"));
     }
 
 
